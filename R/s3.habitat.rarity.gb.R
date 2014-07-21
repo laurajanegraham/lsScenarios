5817f7439f5d693fd673e55209c25a4445485a3b
Laura Graham <lgxlg3@nottingham.ac.uk>
2014-07-21 20:13:06 +0100

    Add in code for habitat rarity fragmentation scenarios (S3 & S4)

---
+++ R/s3.habitat.rarity.gb.R
@@ -0,0 +1,28 @@
+# function samples a landscape of suitable habitat and stops when it has removed the set proportion
+# of suitable habitat patches. Weights patches by type - grassland more vulnerable.
+s3 <- function(lcm.hab) {
+  load("data/habitat.rarity.gb.rda")
+  lcm.gb.stat$rarity <- row.names(lcm.gb.stat)
+  lcm.hab@data <- data.frame(lcm.hab@data, lcm.gb.stat[match(lcm.hab@data$lcm_class, 
+                                                                         lcm.gb.stat$code),])
+  lcm.hab <- lcm.hab[order(lcm.hab$rarity),]
+  lcm.hab$cumsum <- cumsum(lcm.hab$area_ha)
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.1*sum(lcm.hab$area_ha),], 
+           "output", "s3_90_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.2*sum(lcm.hab$area_ha),], 
+           "output", "s3_80_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.3*sum(lcm.hab$area_ha),], 
+           "output", "s3_70_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.4*sum(lcm.hab$area_ha),], 
+           "output", "s3_60_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.5*sum(lcm.hab$area_ha),], 
+           "output", "s3_50_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.6*sum(lcm.hab$area_ha),], 
+           "output", "s3_40_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.7*sum(lcm.hab$area_ha),], 
+           "output", "s3_30_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.8*sum(lcm.hab$area_ha),], 
+           "output", "s3_20_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.9*sum(lcm.hab$area_ha),], 
+           "output", "s3_10_perc", driver="ESRI Shapefile")
+}
---
+++ R/s4.habitat.rarity.nott.R
@@ -0,0 +1,28 @@
+# function samples a landscape of suitable habitat and stops when it has removed the set proportion
+# of suitable habitat patches. Weights patches by type - grassland more vulnerable.
+s4 <- function(lcm.hab) {
+  load("data/habitat.rarity.nott.rda")
+  lcm.stat$rarity <- row.names(lcm.gb.stat)
+  lcm.hab@data <- data.frame(lcm.hab@data, lcm.stat[match(lcm.hab@data$lcm_class, 
+                                                                         lcm.stat$code),])
+  lcm.hab <- lcm.hab[order(lcm.hab$rarity),]
+  lcm.hab$cumsum <- cumsum(lcm.hab$area_ha)
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.1*sum(lcm.hab$area_ha),], 
+           "output", "s4_90_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.2*sum(lcm.hab$area_ha),], 
+           "output", "s4_80_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.3*sum(lcm.hab$area_ha),], 
+           "output", "s4_70_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.4*sum(lcm.hab$area_ha),], 
+           "output", "s4_60_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.5*sum(lcm.hab$area_ha),], 
+           "output", "s4_50_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.6*sum(lcm.hab$area_ha),], 
+           "output", "s4_40_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.7*sum(lcm.hab$area_ha),], 
+           "output", "s4_30_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.8*sum(lcm.hab$area_ha),], 
+           "output", "s4_20_perc", driver="ESRI Shapefile")
+  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.9*sum(lcm.hab$area_ha),], 
+           "output", "s4_10_perc", driver="ESRI Shapefile")
+}
