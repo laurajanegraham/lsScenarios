0cee849d5ffb7857c362e78636f2feeed7943299
Laura Graham <lgxlg3@nottingham.ac.uk>
2014-07-21 19:05:30 +0100

    Correction to output path for S1 & S2 code

--- R/s1.smallest.R
+++ R/s1.smallest.R
@@ -4,22 +4,22 @@ s1 <- function(lcm.hab) {
   lcm.hab <- lcm.hab[order(lcm.hab$area_ha),]
   lcm.hab$cumsum <- cumsum(lcm.hab$area_ha)
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.1*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_90_perc", driver="ESRI Shapefile")
+           "output", "s1_90_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.2*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_80_perc", driver="ESRI Shapefile")
+           "output", "s1_80_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.3*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_70_perc", driver="ESRI Shapefile")
+           "output", "s1_70_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.4*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_60_perc", driver="ESRI Shapefile")
+           "output", "s1_60_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.5*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_50_perc", driver="ESRI Shapefile")
+           "output", "s1_50_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.6*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_40_perc", driver="ESRI Shapefile")
+           "output", "s1_40_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.7*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_30_perc", driver="ESRI Shapefile")
+           "output", "s1_30_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.8*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_20_perc", driver="ESRI Shapefile")
+           "output", "s1_20_perc", driver="ESRI Shapefile")
   writeOGR(lcm.hab[lcm.hab$cumsum >= 0.9*sum(lcm.hab$area_ha),], 
-           "data/shapes/scenarios", "s3_10_perc", driver="ESRI Shapefile")
+           "output", "s1_10_perc", driver="ESRI Shapefile")
   
 }
