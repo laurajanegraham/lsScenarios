8acac20ec4f84d0c889f739e1617a5cceb021579
Laura Graham <lgxlg3@nottingham.ac.uk>
2014-07-22 20:17:34 +0100

    Update names of lcm to match rest of code

--- R/create.scenarios.R
+++ R/create.scenarios.R
@@ -8,6 +8,7 @@ lcm.hab$area_ha <- gArea(lcm.hab, byid=TRUE)/10000
 lcm.hab <- lcm.hab[lcm.hab$area_ha >= 0.02,]
 lcm.hab <- subset(lcm.hab, select=c("INTCODE", "area_ha"))
 
+names(lcm.hab) <- c("lcm_class", "area_ha")
 source("R/s1.smallest.R")
 source("R/s2.largest.R")
 source("R/s3.habitat.rarity.gb.R")
