f64718a942aaca9922d43477f456ad18283bd09a
laurajanegraham <lgxlg3@nottingham.ac.uk>
2014-09-03 19:02:10 +0100

    Change local habitat rarity to update adaptively (rarity order updates after each removal of patches)

--- R/s4.habitat.rarity.nott.R
+++ R/s4.habitat.rarity.nott.R
@@ -1,30 +1,20 @@
 # function samples a landscape of suitable habitat and stops when it has removed the set proportion
 # of suitable habitat patches. Weights patches by type - grassland more vulnerable.
 s4 <- function(lcm.hab) {
-  load("data/habitat.rarity.nott.rda")
-  lcm.stat$rarity <- row.names(lcm.stat)
-  lcm.hab@data <- data.frame(lcm.hab@data, lcm.stat[match(lcm.hab@data$lcm_class, 
-                                                                         lcm.stat$code),])
-  lcm.hab <- lcm.hab[order(as.numeric(lcm.hab$rarity)),]
-  lcm.hab@data <- subset(lcm.hab@data, select=c("code", "area_ha"))
-  names(lcm.hab@data)  <- c("lcm_class", "area_ha")
-  lcm.hab$cumsum <- cumsum(lcm.hab$area_ha)
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.1*sum(lcm.hab$area_ha),], 
-           "output", "s4_90_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.2*sum(lcm.hab$area_ha),], 
-           "output", "s4_80_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.3*sum(lcm.hab$area_ha),], 
-           "output", "s4_70_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.4*sum(lcm.hab$area_ha),], 
-           "output", "s4_60_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.5*sum(lcm.hab$area_ha),], 
-           "output", "s4_50_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.6*sum(lcm.hab$area_ha),], 
-           "output", "s4_40_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.7*sum(lcm.hab$area_ha),], 
-           "output", "s4_30_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.8*sum(lcm.hab$area_ha),], 
-           "output", "s4_20_perc", driver="ESRI Shapefile")
-  writeOGR(lcm.hab[lcm.hab$cumsum >= 0.9*sum(lcm.hab$area_ha),], 
-           "output", "s4_10_perc", driver="ESRI Shapefile")
+  require(dplyr)
+  total.area <- sum(lcm.hab$area_ha)
+  for(p in seq(10, 90, 10)){
+    lcm.stat <- ddply(lcm.hab@data, .(lcm_class), summarise, area_ha=sum(area_ha))
+    lcm.stat <- arrange(lcm.stat, -area_ha)
+    lcm.stat$rarity <- row.names(lcm.stat)
+    lcm.hab@data <- data.frame(lcm.hab@data, lcm.stat[match(lcm.hab@data$lcm_class, 
+                                                            lcm.stat$lcm_class),])
+    lcm.hab <- lcm.hab[order(as.numeric(lcm.hab$rarity)),]
+    lcm.hab@data <- subset(lcm.hab@data, select=c("lcm_class", "area_ha"))
+    names(lcm.hab@data)  <- c("lcm_class", "area_ha")
+    lcm.hab$cumsum <- cumsum(lcm.hab$area_ha)
+    lcm.hab <- lcm.hab[lcm.hab$cumsum >= 0.1*total.area,]
+    writeOGR(lcm.hab, "output", paste0("s4_", 100-p,"_perc"), driver="ESRI Shapefile")
+  }
+  
 }
